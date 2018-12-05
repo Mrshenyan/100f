07d639a6eb5e3ef81789ccb5be6e717186c410f1
1_MrDc <2895289240tay@gmail.com>
2018-12-05 09:15:43 +0800

    已经能够加载结束场景的动画

---
+++ assets/resources/Prefab/fuhuo1.ts
@@ -0,0 +1,42 @@
+
+const {ccclass, property} = cc._decorator;
+
+@ccclass
+export default class NewClass extends cc.Component {
+
+    // LIFE-CYCLE CALLBACKS:
+
+    onLoad () {
+        
+    }
+
+    start () {
+
+    }
+
+    update (dt) {
+        let self = this;
+        let Ani;
+        let Anistate;
+        let Countdown = this.node.getChildByName("fh").getChildByName("Countdown").getComponent(cc.Label);
+        let time = parseInt(Countdown.string);
+        let scheduler = cc.director.getScheduler();
+        scheduler.schedule(aniplayer,this,1,false);
+
+        function aniplayer(){
+            let self = this;
+            if(time>0){
+                time--;
+            }
+            if(time==0){
+                this.isScheduled = false;
+                Ani = self.getComponent(cc.Animation);
+                Anistate = Ani.play();
+                Countdown.string = time.toString();
+                scheduler.pauseTarget(this); 
+                return;
+            }
+            Countdown.string = time.toString();
+        }
+    }
+}
